cf316169e8ea855c630627ca7d7a28b65082df81
KudesnikRaf <kolesnik.akk@gmail.com>
2025-03-07 01:30:33 +0300

    Updates

--- src/components/toBroadCast.jsx
+++
@@ -1,9 +0,0 @@
-function toBroadcastAddress(network, wildcard) {
-  const networkParts = network.split('.').map(Number);
-  const wildcardParts = wildcard.split('.').map(Number);
-
-  const broadcastParts = networkParts.map((part, index) => part | wildcardParts[index]);
-  return broadcastParts.join('.');
-}
-
-export default toBroadcastAddress;
